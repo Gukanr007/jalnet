2381f1701ac491c620984dc0062bedf93f28e05b
Gukanr007 <gukanranganathan@gmail.com>
2025-05-28 10:14:07 +0530

    fixed deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// // import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   // server: {
+//   //   host: "::",
+//   //   port: 8080,
+//   // },
+//   plugins: [react()],
+//   base: '/jalnet/',
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+export default defineConfig({
+  base: '/jalnet/', // IMPORTANT for GitHub Pages
   plugins: [react()],
-  base: '/jalnet/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-}));
+})
