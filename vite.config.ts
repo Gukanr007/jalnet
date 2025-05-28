05f3a0fae6de13115a38f7bb9cc89c31967e1615
Gukanr007 <gukanranganathan@gmail.com>
2025-05-28 08:35:32 +0530

    deploy fixes

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/jalnet",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
