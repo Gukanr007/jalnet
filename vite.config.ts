c8092941315a02283efb3b1741165865bdb5447f
Gukanr007 <gukanranganathan@gmail.com>
2025-07-19 11:06:56 +0530

    /

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: '/jalnet/', // IMPORTANT for GitHub Pages
+  base: '/', // Set to root for Netlify deployment
   plugins: [react()],
   resolve: {
     alias: {
